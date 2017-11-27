b8ab42d09920aa38424c27c0ad058a922ae428d6
Mattias Hammarhorn <mattias@hammarhorn.se>
2017-11-27 14:03:02 +0100

    First commit

---
+++ Uppgift1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter names separated by comma (e.g Maria,Peter,Lisa): ");
+            string userInput = Console.ReadLine();
+            splitAndLoopThroughInput(userInput);
+        }
+
+        public static void splitAndLoopThroughInput(string userInput)
+        {
+            string[] inputToBeParsed = userInput.Split(',');
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (string name in inputToBeParsed)
+            {
+                Console.WriteLine("***" +  "SUPER-" + name.ToUpper() + "***");
+            }
+            Console.ResetColor();
+        }
+    }
+}
